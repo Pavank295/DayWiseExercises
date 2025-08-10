8cc7076e81889767a59503c9bd0547b5ad6e6274
Bojanapu Pavankumar <pavankumarbojanapu3@gmail.com>
2025-08-10 21:40:13 +0530

    Delete letsgo/letsgo directory

--- letsgo/letsgo/src/main/java/com/wipro/letsgo/dto/VehicleMovementRequest.java
+++
@@ -1,8 +0,0 @@
-package com.wipro.letsgo.dto;
-import lombok.Data;
-@Data
-public class VehicleMovementRequest {
-    private Long vehId;
-    private Double lat;
-    private Double longitude; 
-}
--- letsgo/letsgo/src/main/java/com/wipro/letsgo/repository/VehicleMovementRepository.java
+++
@@ -1,5 +0,0 @@
-package com.wipro.letsgo.repository;
-import com.wipro.letsgo.entity.VehicleMovement;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface VehicleMovementRepository extends JpaRepository<VehicleMovement, Long> {
-}
