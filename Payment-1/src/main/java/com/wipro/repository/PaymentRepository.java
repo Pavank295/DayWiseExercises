1e7022958ee382e6d13043d24e68d38713f457a4
Bojanapu Pavankumar <pavankumarbojanapu3@gmail.com>
2025-08-10 21:38:06 +0530

    Delete Payment-1 directory

--- Payment-1/src/main/java/com/wipro/repository/PaymentRepository.java
+++
@@ -1,4 +0,0 @@
-package com.wipro.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.wipro.entity.Payment;
-public interface PaymentRepository extends JpaRepository<Payment, Integer> {}
